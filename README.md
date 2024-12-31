fa273ed2a30d0f63e67864229898d99fb8b46410
Neil Van Alino <nvq.alino@gmail.com>
2024-12-31 11:53:23 +0800

    init

---
+++ README.md
@@ -0,0 +1,41 @@
+# map-ctlr
+
+Navigate maps using a gamepad.
+
+## Mapping of the physical gamepad to the Gamepad API
+
+*Note: Microsoft X-Box 360 pad nomenclature*
+
+*Reference: [Xbox controller](https://gist.github.com/palmerj/586375bcc5bc83ccdaf00c6f5f863e86)*
+
+![gamepad](https://user-images.githubusercontent.com/580022/45268303-10a03e80-b4ce-11e8-883c-1f586566c040.png)
+
+### Sticks
+
+LSB: axes[0], axes[1] (leftRightAxis, upDownAxis)
+
+RSB: axes[2], axes[3] (leftRightAxis, upDownAxis)
+
+### Buttons
+
+A: buttons[0]
+
+B: buttons[1]
+
+Y: buttons[2]
+
+X: buttons[3]
+
+LB: buttons[4]
+
+RB: buttons[5]
+
+### D-PAD
+
+Up: buttons[12]
+
+Down: buttons[13]
+
+Left: buttons[14]
+
+Right: buttons[15]
---
+++ static/index.js
@@ -0,0 +1,102 @@
+function initMap() {
+  map = L.map("map").setView([37.7749, -122.4194], 12); // San Francisco
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 40,
+    attribution: "© OpenStreetMap contributors",
+  }).addTo(map);
+}
+
+let controllerIndex = null;
+let isZoomingIn = false;
+let isZoomingOut = false;
+
+window.addEventListener("gamepadconnected", (event) => {
+  const gamepad = event.gamepad;
+  controllerIndex = gamepad.index;
+  console.log("connected", gamepad.id);
+});
+
+window.addEventListener("gamepaddisconnected", (event) => {
+  controllerIndex = null;
+  console.log("disconnected");
+});
+
+function handleButtons(buttons) {
+  const currentZoom = map.getZoom();
+
+  let step;
+  if (currentZoom > 19) {
+    step = 0.00001;
+  } else if (currentZoom > 16) {
+    step = 0.0001;
+  } else if (currentZoom > 12) {
+    step = 0.001;
+  } else if (currentZoom > 9) {
+    step = 0.01;
+  } else if (currentZoom > 6) {
+    step = 0.1;
+  } else {
+    step = 1;
+  }
+
+  const dPadIdx = [12, 13, 14, 15];
+  let newCenter = null;
+
+  for (let i = 0; i < buttons.length; i++) {
+    if (dPadIdx.includes(i) && buttons[i].pressed) {
+      const currentCenter = map.getCenter();
+
+      if (i === 12) {
+        // D-pad up
+        newCenter = [currentCenter.lat + step, currentCenter.lng];
+      } else if (i === 13) {
+        // D-pad down
+        newCenter = [currentCenter.lat - step, currentCenter.lng];
+      } else if (i === 14) {
+        // D-pad left
+        newCenter = [currentCenter.lat, currentCenter.lng - step];
+      } else if (i === 15) {
+        // D-pad right
+        newCenter = [currentCenter.lat, currentCenter.lng + step];
+      }
+    }
+  }
+
+  // Handle zoom buttons
+  if (buttons[4].pressed && !isZoomingOut) {
+    // Left button for zoom out
+    map.zoomOut();
+    isZoomingOut = true;
+  } else if (!buttons[4].pressed) {
+    isZoomingOut = false;
+  }
+
+  if (buttons[5].pressed && !isZoomingIn) {
+    // Right button for zoom in
+    map.zoomIn();
+    isZoomingIn = true;
+  } else if (!buttons[5].pressed) {
+    isZoomingIn = false;
+  }
+
+  return newCenter;
+}
+
+function gameLoop() {
+  if (controllerIndex !== null) {
+    const gamepad = navigator.getGamepads()[controllerIndex];
+
+    const newCenter = handleButtons(gamepad.buttons);
+    if (newCenter) {
+      map.panTo(newCenter, { animate: false });
+    }
+  }
+
+  requestAnimationFrame(gameLoop);
+}
+
+window.onload = function () {
+  initMap();
+  gameLoop();
+};
